0c110999c1f11d165ae4fdfbe33ccc044af6e856
jecka1997 <jecka1997.zoe@gmail.com>
2019-08-17 17:37:45 -0500

    desarrollo de la navegacion

--- index.py
+++ index.py
@@ -9,10 +9,20 @@ def home():
     #desplega el contenido de la pagina web home.html
     return render_template('home.html')
 
+@app.route('/diseños')
+def diseños():
+    #desplega el contenido de la pagina web diseños.html
+    return render_template('diseños.html')
+
 @app.route('/acerca')
 def acerca():
     #desplega el contenido de la pagina web about.html
     return render_template('about.html')
+
+@app.route('/contacto')
+def contacto():
+    #desplega el contenido de la pagina web contacto.html
+    return render_template('contacto.html')
 
 if __name__ =='__main__':
     #se encarga de ejecutar el servidor
